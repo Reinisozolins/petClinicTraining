927614f335483e63748ec5421064e01952181fd3
Reinis Ozo <reinisozolins@mail.com>
2020-09-17 23:58:59 +0300

    POJO data model

---
+++ pet-clinic-web/src/main/java/PetClinicTrainingApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/ozo/petClinicTraining/PetClinicTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.ozo.petClinicTraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
